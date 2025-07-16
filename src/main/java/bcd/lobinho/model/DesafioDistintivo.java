08169a6db191e22a71b6d7e0dea231ce887389de
juliaturness <jmturnes10@gmail.com>
2025-07-16 16:13:59 -0300

    fazendo inicialmente algumas entidades

--- src/main/java/bcd/lobinho/model/DesafioDistintivo.java
+++ src/main/java/bcd/lobinho/model/DesafioDistintivo.java
@@ -1,4 +1,22 @@
 package bcd.lobinho.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "desafio_distintivo")
 public class DesafioDistintivo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_distintivo")
+    private Distintivo distintivo;
+
+    @OneToMany(mappedBy = "desafioDistintivo")
+    private List<DesafioDistintivoFeito> realizacoes;
 }
--- src/main/java/bcd/lobinho/model/DesafioDistintivoFeito.java
+++ src/main/java/bcd/lobinho/model/DesafioDistintivoFeito.java
@@ -1,4 +1,35 @@
 package bcd.lobinho.model;
 
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "desafio_distintivo_feito")
 public class DesafioDistintivoFeito {
+    @EmbeddedId
+    private DesafioDistintivoFeitoId id;
+
+    @Column(name = "data_inicio")
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    private LocalDate dataFim;
+
+    @ManyToOne
+    @MapsId("idDesafioDistintivo")
+    @JoinColumn(name = "id_desafio_distintivo")
+    private DesafioDistintivo desafioDistintivo;
+
+    @ManyToOne
+    @MapsId("idPessoa")
+    @JoinColumn(name = "id_pessoa")
+    private Pessoa pessoa;
+}
+
+
+@Embeddable
+class DesafioDistintivoFeitoId implements Serializable {
+    private Integer idDesafioDistintivo;
+    private Integer idPessoa;
 }
